806e37dad0ee70efdd2d4ff08d8da19324c8824d
Marcelo Olivas <mfolivas@gmail.com>
2018-08-21 12:07:35 -0400

    got the unit tests passed

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,69 +1,70 @@
-'use strict';
+const fs = require("fs")
+const Mustache = require('mustache')
+const http = require('superagent-promise')(require('superagent'), Promise)
+const aws4 = require('aws4')
+const URL = require('url')
 
-const co = require("co");
-const Promise = require("bluebird");
-const fs = Promise.promisifyAll(require("fs"));
-const Mustache = require('mustache');
-const http = require('superagent-promise')(require('superagent'), Promise);
-const aws4 = require('aws4');
-const URL = require('url');
+const restaurantsApiRoot = process.env.restaurants_api
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-const awsRegion = process.env.AWS_REGION;
-const cognitoUserPoolId = process.env.cognito_user_pool_id;
-const cognitoClientId = process.env.cognito_client_id;
+const awsRegion = process.env.AWS_REGION
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
 
-const restaurantsApiRoot = process.env.restaurants_api;
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let html
 
-var html;
-
-function* loadHtml() {
+function loadHtml () {
   if (!html) {
-    html = yield fs.readFileAsync('static/index.html', 'utf-8');
+    console.log('loading index.html...')
+    html = fs.readFileSync('static/index.html', 'utf-8')
+    console.log('loaded')
   }
-
-  return html;
+  
+  return html
 }
 
-function* getRestaurants() {
-  let url = URL.parse(restaurantsApiRoot);
-  let opts = {
-    host: url.hostname,
+const getRestaurants = async () => {
+  const url = URL.parse(restaurantsApiRoot)
+  const opts = {
+    host: url.hostname, 
     path: url.pathname
-  };
+  }
 
-  aws4.sign(opts);
+  aws4.sign(opts)
 
-  return (yield http
+  const httpReq = http
     .get(restaurantsApiRoot)
     .set('Host', opts.headers['Host'])
     .set('X-Amz-Date', opts.headers['X-Amz-Date'])
     .set('Authorization', opts.headers['Authorization'])
-    .set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token'])).body
+    
+  if (opts.headers['X-Amz-Security-Token']) {
+    httpReq.set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token'])
+  }
+
+  return (await httpReq).body
 }
 
-module.exports.handler = co.wrap(function* (event, context, callback) {
-  let template = yield loadHtml();
-  let restaurants = yield getRestaurants();
-  let dayOfWeek = days[new Date().getDay()];
-  let view = {
-    dayOfWeek,
-    restaurants,
+module.exports.handler = async (event, context) => {
+  const template = loadHtml()
+  const restaurants = await getRestaurants()
+  const dayOfWeek = days[new Date().getDay()]
+  const view = { 
     awsRegion,
     cognitoUserPoolId,
     cognitoClientId,
+    dayOfWeek, 
+    restaurants,
     searchUrl: `${restaurantsApiRoot}/search`
   }
-
-  let html = Mustache.render(template, view);
-
+  const html = Mustache.render(template, view)
   const response = {
     statusCode: 200,
-    body: html,
     headers: {
-      'Content-Type' : 'text/html; charset=UTF-8'
-    }
-  };
+      'Content-Type': 'text/html; charset=UTF-8'
+    },
+    body: html
+  }
 
-  callback(null, response);
-});
+  return response
+}
--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,31 +1,29 @@
-'use strict'
+const AWS = require('aws-sdk')
+const dynamodb = new AWS.DynamoDB.DocumentClient()
 
-const co = require('co');
-const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const defaultResults = process.env.defaultResults || 8
+const tableName = process.env.restaurants_table
 
-const defaultResults = process.env.defaultResults || 8;
-const tableName = process.env.restaurants_table;
+const findRestaurantsByTheme = async (theme, count) => {
+  const req = {
+    TableName: tableName,
+    Limit: count,
+    FilterExpression: "contains(themes, :theme)",
+    ExpressionAttributeValues: { ":theme": theme }
+  }
 
-function* findRestaurantsByTheme(theme, count) {
-    let req = {
-        TableName: tableName,
-        Limit: count,
-        FilterExpression: "contains(themes, :theme)",
-        ExpressionAttributeValues: { ":theme": theme }
-    };
-
-    let resp = yield dynamodb.scan(req).promise();
-    return resp.Items;
+  const resp = await dynamodb.scan(req).promise()
+  return resp.Items
 }
 
-module.exports.handler = co.wrap(function* (event, context, cb) {
-    let req = JSON.parse(event.body);
-    let restaurants = yield findRestaurantsByTheme(req.theme, defaultResults);
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify(restaurants)
-    }
+module.exports.handler = async (event, context) => {
+  const req = JSON.parse(event.body)
+  const theme = req.theme
+  const restaurants = await findRestaurantsByTheme(theme, defaultResults)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(restaurants)
+  }
 
-    cb(null, response);
-});
+  return response
+}
